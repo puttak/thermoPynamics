260e69b5133c591279804b5bc716aef0068943b8
msscaroso <mssc.engq@gmail.com>
2017-09-04 14:02:37 -0300

    Marco zero da implementação do UNIFAC

---
+++ ThermoPkgs/UNIFAC/UNIFAC.py
@@ -0,0 +1,10 @@
+#coding: utf-8
+import numpy as np
+
+
+class UNIFAC:
+    def __init__(self):
+        pass
+
+    def computeGama(self, T, x):
+        return
---
+++ ThermoPkgs/UNIFAC/testUNIFAC.py
@@ -0,0 +1,6 @@
+#coding: utf-8
+
+from UNIFAC import UNIFAC
+
+a=UNIFAC()
+print a.computeGama(300, [0.1])
