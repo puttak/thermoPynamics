f1edf99006a0d2e695ccb6bd8c6074dbda67de45
msscaroso <mssc.engq@gmail.com>
2017-09-05 12:14:05 -0300

    desenvolvimento leitura de dados do DB UNIFAC

--- ThermoPkgs/UNIFAC/GetData.py
+++ ThermoPkgs/UNIFAC/GetData.py
@@ -18,12 +18,16 @@ class ReadDataUNIFAC:
         self.__cursor = conector.cursor()
 
     def GetComponentGroupSpecification(self):
+        #cria matriz v_k e matriz G ([mainGroupNumber,[subgrupos relacionados], ..., [mainGroupNumber,[subgrupos relacionados] ]
 
 
         for id in self.ID:
             self.__cursor.execute('SELECT groupID, subGroupID, NumberOfOccurrences '
                                   'FROM [UNIFAC_component_group_specifications] WHERE componentID=%d' % (id))
             row=self.__cursor.fetchall()
+            #pegar todos os sub grupos de todos os componentes em vetores de dim variáveis.
+            #pegar o número total de sg diferentes. Armazenar vk em um vetor com dim NG e organizar como na folha.
+
             #TODO: OLHAR COMO E A MATRIZ COM OS GRUPOS EM THERMO E CRIAR UMA MATRIZ SEMELHANTE AQUI.
             #TODO: ARMAZENAR EM UMA MATRIZ O NÚMERO DE OCORRÊNCIA E NÚMERO DO SUBGRUPO.
 
--- validations/main.py
+++ validations/main.py
@@ -1,17 +1,22 @@
 #coding: utf-8
 from ThermoPkgs.PR.PR import PR
 from ThermoPkgs.PR.interface import FluidData, Fluid
-
+import numpy as np
 ##############
 ## Aqui será colocado os testes em geral. Validações são adicionadas separadamente quando um novo módulo é terminado. ####
 #TODO: Olhar TODOS
 
+#
+#
+# fluido=Fluid(ID=[31,47], z=[0.5, 0.5])
+# fdata=FluidData(fluido)
+#
+# eos=PR(fluido, fdata)
+#
+# print eos.computeFUG(300, 1, 'vapor')
+#
 
-
-fluido=Fluid(ID=[31,47], z=[0.5, 0.5])
-fdata=FluidData(fluido)
-
-eos=PR(fluido, fdata)
-
-print eos.computeFUG(300, 1, 'vapor')
-
+xi=np.array(range(11))
+li=np.array(range(11))
+print xi, li, xi*li
+print sum(xi*li)
