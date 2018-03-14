21f805781ff08504450a5eb701ad8b77120cb97e
msscaroso <mssc.engq@gmail.com>
2018-03-14 00:37:16 -0300

    Update example.py

--- example.py
+++ example.py
@@ -9,7 +9,7 @@ y=[0.79942,0.05029,0.03000,0.02090,0.09939] #Molar composition of the gas.
 
 a=JT(300, 300, ID, y,fase='vapor',flagEOS='SRK')    # (T1 -> Temperature before expansion , P1-> Pressure before expansion, ID, y, fase -> vapor or liquid, flagEOS->C-EOS flag)
 
-#SRK stands sor SRK cubic equations of state.
+#SRK stands for SRK cubic equations of state.
 
 #
 print a.computeT2(280.0) #T2=298.531558557
